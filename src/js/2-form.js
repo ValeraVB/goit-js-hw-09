f1546aac3ace98941d16f8089f9d3e8779925636
Valeriy <vb@profish.com.ua>
2024-06-22 23:18:36 +0300

    add home work 9.2

---
+++ src/js/2-form.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const formData = {
+    email: '',
+    message: '',
+  };
+
+  const form = document.querySelector('.feedback-form');
+  const emailInput = form.querySelector('input[name="email"]');
+  const messageInput = form.querySelector('textarea[name="message"]');
+
+  // Перевіряємо локальне сховище при завантаженні сторінки
+  const storedData = localStorage.getItem('feedback-form-state');
+  if (storedData) {
+    const parsedData = JSON.parse(storedData);
+    formData.email = parsedData.email;
+    formData.message = parsedData.message;
+
+    emailInput.value = formData.email;
+    messageInput.value = formData.message;
+  }
+
+  // Слухаємо події input для зберігання даних в локальне сховище
+  form.addEventListener('input', function (e) {
+    const { name, value } = e.target;
+    formData[name] = value.trim(); // обрізаємо пробіли з початку і кінця
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+  });
+
+  // Обробляємо подію submit форми
+  form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    // Перевіряємо, чи заповнені обидва поля форми
+    if (formData.email && formData.message) {
+      console.log(formData); // Виводимо formData в консоль
+      localStorage.removeItem('feedback-form-state'); // Очищаємо локальне сховище
+      form.reset(); // Скидаємо значення полів форми
+      formData.email = '';
+      formData.message = '';
+    } else {
+      alert('Будь ласка, заповніть обидва поля');
+    }
+  });
+});
