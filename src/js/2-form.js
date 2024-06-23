9ac99dc7f91b8e51a541d72519a381777b0aa698
Valeriy <vb@profish.com.ua>
2024-06-23 17:33:29 +0300

    add home work 9

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -20,15 +20,15 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
   // Слухаємо події input для зберігання даних в локальне сховище
-  form.addEventListener('input', function (e) {
-    const { name, value } = e.target;
+  form.addEventListener('input', function (evt) {
+    const { name, value } = evt.target;
     formData[name] = value.trim(); // обрізаємо пробіли з початку і кінця
     localStorage.setItem('feedback-form-state', JSON.stringify(formData));
   });
 
   // Обробляємо подію submit форми
-  form.addEventListener('submit', function (e) {
-    e.preventDefault();
+  form.addEventListener('submit', function (evt) {
+    evt.preventDefault();
 
     // Перевіряємо, чи заповнені обидва поля форми
     if (formData.email && formData.message) {
